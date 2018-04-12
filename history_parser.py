df4668d437456445a124c849ce3ad3d0b8083468
Michael Trittin <trittimo@rose-hulman.edu>
2018-04-12 18:02:36 -0400

    Initial commit

---
+++ history_parser.py
@@ -0,0 +1,25 @@
+import json
+with open("history.json", "r") as f:
+  history = json.loads(f.read())
+
+locations = {"type": "FeatureCollection", "features": []}
+
+
+limit = 10
+for loc in history["locations"]:
+  if limit <= 0:
+    break
+  limit -= 1
+  locations["features"].append({
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        loc["latitudeE7"] / (10**7),
+        loc["longitudeE7"] / (10**7)
+      ]
+    }
+  })
+
+with open("history_features.json", "w") as f:
+  f.write(json.dumps(locations))
